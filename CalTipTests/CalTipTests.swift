4dd9670a94695fb0790dbd64fc3a89544a28e7e6
Suhayl SH <suhayl-imac@test1-pc.ingpdc03.gp.cv.commvault.com>
2017-02-14 10:08:43 +0530

    Initial Commit

---
+++ CalTipTests/CalTipTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalTipTests.swift
+//  CalTipTests
+//
+//  Created by Suhayl SH on 14/02/17.
+//  Copyright © 2017 Suhayl SH. All rights reserved.
+//
+
+import XCTest
+@testable import CalTip
+
+class CalTipTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
